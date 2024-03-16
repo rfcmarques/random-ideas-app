525fedbaccbc75753014ec403f62b48b8ae3cfc8
Rui Marques <a.marques1967@gmail.com>
2024-03-16 19:29:01 +0000

    Added the function to add new ideas with the client

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -1,32 +1,38 @@
+import IdeasApi from "../services/IdeasApi";
+import IdeaList from "./IdeaList";
+
 class IdeaForm {
-    constructor() {
-        this._formModal = document.querySelector('#form-modal');
+  constructor() {
+    this._formModal = document.querySelector('#form-modal');
+    this._ideaList = new IdeaList();
+  }
+
+  addEventListeners() {
+    this._form.addEventListener('submit', this.handleSubmit.bind(this));
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const idea = {
+      text: this._form.elements.text.value,
+      tag: this._form.elements.tag.value,
+      username: this._form.elements.username.value,
     }
 
-    addEventListeners() {
-        this._form.addEventListener('submit', this.handleSubmit.bind(this));
-    }
+    const newIdea = await IdeasApi.createIdea(idea);
 
-    handleSubmit(e) {
-        e.preventDefault();
+    this._ideaList.addIdeaToList(newIdea.data.data);
 
-        const idea = {
-            text: this._form.elements.text.value,
-            tag: this._form.elements.tag.value,
-            username: this._form.elements.username.value,
-        }
+    this._form.elements.text.value = '';
+    this._form.elements.tag.value = '';
+    this._form.elements.username.value = '';
 
-        console.log(idea);
+    document.dispatchEvent(new Event('closemodal'));
+  }
 
-        this._form.elements.text.value = '';
-        this._form.elements.tag.value = '';
-        this._form.elements.username.value = '';
-
-        document.dispatchEvent(new Event('closemodal'));
-    }
-
-    render() {
-        this._formModal.innerHTML = `
+  render() {
+    this._formModal.innerHTML = `
         <form id="idea-form">
         <div class="form-control">
           <label for="idea-text">Enter a Username</label>
@@ -44,9 +50,9 @@ class IdeaForm {
       </form>
         `;
 
-        this._form = document.querySelector('#idea-form');
-        this.addEventListeners();
-    }
+    this._form = document.querySelector('#idea-form');
+    this.addEventListeners();
+  }
 }
 
 export default IdeaForm;
