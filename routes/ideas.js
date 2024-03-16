ba357be1291ee3a35301c160bf576a14ba8d6a95
Rui Marques <a.marques1967@gmail.com>
2024-03-16 16:25:50 +0000

    Added full CRUD functions to the ideas

--- routes/ideas.js
+++ routes/ideas.js
@@ -27,7 +27,21 @@ const ideas = [
 
 router.get('/', (req, res) => {
     res.json({ success: true, data: ideas });
-})
+});
+
+router.post('/', (req, res) => {
+    const idea = {
+        id: ideas.length + 1,
+        text: req.body.text,
+        tag: req.body.tag,
+        username: req.body.username,
+        date: new Date().toISOString().slice(0, 10),
+    }
+
+    ideas.push(idea);
+
+    res.json({ success: true, data: idea });
+});
 
 router.get('/:id', (req, res) => {
     const idea = ideas.find(idea => idea.id === +req.params.id);
@@ -39,6 +53,35 @@ router.get('/:id', (req, res) => {
     }
 
     res.json({ success: true, data: idea });
-})
+});
+
+router.put('/:id', (req, res) => {
+    const idea = ideas.find(idea => idea.id === +req.params.id);
+
+    if (!idea) {
+        return res
+            .status(404)
+            .json({ success: false, error: 'Resource not found' });
+    }
+
+    idea.text = req.body.text || idea.text;
+    idea.tag = req.body.tag || idea.tag;
+
+    res.json({ success: true, data: idea });
+});
+
+router.delete('/:id', (req, res) => {
+    const idea = ideas.find(idea => idea.id === +req.params.id);
+
+    if (!idea) {
+        return res
+            .status(404)
+            .json({ success: false, error: 'Resource not found' });
+    }
+
+    ideas.splice(ideas.indexOf(idea), 1);
+
+    res.json({ success: true, data: {} });
+});
 
 module.exports = router;
