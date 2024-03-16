ab73980df24e8b4fba7484327252b70f074e5211
Rui Marques <a.marques1967@gmail.com>
2024-03-16 16:11:16 +0000

    Cleaned up the routes to a diferent file

--- server.js
+++ server.js
@@ -2,48 +2,12 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
-const ideas = [
-    {
-        id: 1,
-        text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
-        tag: 'Technology',
-        username: 'TonyStark',
-        date: '2024-03-02',
-    },
-    {
-        id: 2,
-        text: 'Milk cartons that turn a different color the older that your milk is getting',
-        tag: 'Inventions',
-        username: 'SteveRogers',
-        date: '2024-03-02',
-    },
-    {
-        id: 3,
-        text: 'ATM location app wich lets you know where the closest ATM is and if it is in service',
-        tag: 'Software',
-        username: 'BruceBanner',
-        date: '2024-03-02',
-    },
-]
-
 app.get('/', (req, res) => {
     res.json({ message: 'Hello to the RandomIdeas API' });
 });
 
-app.get('/api/ideas', (req, res) => {
-    res.json({ success: true, data: ideas });
-})
+const ideasRouter = require('./routes/ideas');
 
-app.get('/api/ideas/:id', (req, res) => {
-    const idea = ideas.find(idea => idea.id === +req.params.id);
-
-    if (!idea) {
-        return res
-            .status(404)
-            .json({ success: false, error: 'Resource not found' });
-    }
-
-    res.json({ success: true, data: idea });
-})
+app.use('/api/ideas', ideasRouter);
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
